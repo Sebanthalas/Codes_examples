a0ef16dfbe45f62564c799710c7771ca368580c4
Sebastián Moraga Scheuermann <smoragas@sfu.ca>
2024-07-06 19:14:40 -0700

    Update example.py
    edit the input

--- Q2/example.py
+++ Q2/example.py
@@ -6,7 +6,7 @@ Description:   This script contains the phonemes inputs to be used in the main c
 """
 from findwords import *
 # Example usage
-phonemes_input = ["T", "AH", "M", "EY", "T", "OW","T", "AH", "M", "AA", "T", "OW"]
+phonemes_input = ["DH", "EH", "R", "DH", "EH", "R"]
 file_path      = 'pronundic.txt'   #We can replace this for a larger example
 combinations   = find_word_combos_with_pronunciation(phonemes_input, file_path)
 print(combinations)
